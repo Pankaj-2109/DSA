7f7bcef970158dc00b0385ad4f492f0fd3806ff3
Pankaj Singh Bisht <pankajsinghb2109@gmail.com>
2026-08-20 19:39:12 +0530

    Time: 1 ms (6.72%) | Memory: 43.3 MB (33.17%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> pre=new ArrayList<>();
+        if(root==null){
+            return pre;
+        }
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root=st.pop();
+            pre.add(root.val);
+            if(root.right!=null){
+                st.push(root.right);
+            }
+            if(root.left!=null){
+                st.push(root.left);
+            }
+        }
+        return pre;
+
+        
+    }
+}
